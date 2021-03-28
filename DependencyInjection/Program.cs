f241f18a1fc9c98d17bd9febd88531d209ab00ec
Omar Ebrahim <omar.ebrahim1990@outlook.com>
2021-03-28 17:03:34 +0100

    Add project files.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection
+{
+    public interface IPet
+    {
+        void Walk();
+    }
+
+    public class Dog : IPet
+    {
+        public void Walk()
+        {
+            Console.WriteLine("I'm so excited! I'm going for a walk!");
+        }
+    }
+
+    public class Cat : IPet
+    {
+        public void Walk()
+        {
+            Console.WriteLine("Nope, I'm gonna nap...");
+        }
+    }
+
+    public class Hamster : IPet
+    {
+        public void Walk()
+        {
+            Console.WriteLine("I don't walk!");
+        }
+    }
+
+
+    public class PetWalker
+    {
+        readonly IEnumerable<IPet> _pets;
+
+        public PetWalker(IEnumerable<IPet> pets)
+        {
+            _pets = pets;
+        }
+
+        public void WalkPets()
+        {
+            foreach (var pet in _pets)
+            {
+                pet.Walk();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pets = new List<IPet>() { new Cat(), new Dog(), new Hamster() };
+
+            var walker = new PetWalker(pets);
+            walker.WalkPets();
+            Console.Read();
+        }
+    }
+}
